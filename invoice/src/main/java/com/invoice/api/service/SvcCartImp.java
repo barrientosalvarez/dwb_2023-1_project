77878604ff8a05acc1b4f466e9d08978a27d9be8
Barrientos Alvarez Jorge <jma.barrientos@ciencias.unam.mx>
2022-12-13 17:41:40 -0600

    service cart imp updated

--- invoice/src/main/java/com/invoice/api/service/SvcCartImp.java
+++ invoice/src/main/java/com/invoice/api/service/SvcCartImp.java
@@ -37,14 +37,15 @@ public class SvcCartImp implements SvcCart {
 		if(!validateCustomer(cart.getRfc()))
 			throw new ApiException(HttpStatus.BAD_REQUEST, "customer does not exist");
 
-        if(!validateProduct(cart.getGtin()))
-            throw new ApiException(HttpStatus.BAD_REQUEST, "product does not exist");
 			
 		/*
 		 * Requerimiento 3
 		 * Validar que el GTIN exista. Si existe, asignar el stock del producto a la variable product_stock 
 		 */
-		Integer product_stock = productCl.getProduct(cart.getGtin()); // cambiar el valor de cero por el stock del producto recuperado de la API Product 
+        if(!validateProduct(cart.getGtin()))
+            throw new ApiException(HttpStatus.BAD_REQUEST, "product does not exist");
+
+		Integer product_stock = productCl.getProduct(cart.getGtin()).getStock(); // cambiar el valor de cero por el stock del producto recuperado de la API Product 
 		
 		if(cart.getQuantity() > product_stock) {
 			throw new ApiException(HttpStatus.BAD_REQUEST, "invalid quantity");
@@ -54,9 +55,23 @@ public class SvcCartImp implements SvcCart {
 		 * Requerimiento 4
 		 * Validar si el producto ya había sido agregado al carrito para solo actualizar su cantidad
 		 */
-		
+
+        if(repo.findCartById(cart.getCart_id(cart.getCart_id))!=null){
+            Cart oldCart=repo.findCartById(cart.getCart_id);
+
+            if((oldCart.getQuantity()+cart.getQuantity())>product_stock)
+                throw new ApiException(HttpStatus.BAD_REQUEST, "invalid quantity");
+
+            else{
+                newStock=product_stock-(oldCart.getQuantity()+cart.getQuantity());
+                productCl.updateProductStock(cart.getGtin(), newStock);
+                return new ApiResponse("quantity updated");
+            }
+        }
+
 		cart.setStatus(1);
 		repo.save(cart);
+
 		return new ApiResponse("item added");
 	}
 
