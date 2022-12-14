9e36249f7aa8a244ec5c09fc9926ffb6c1de7007
Barrientos Alvarez Jorge <jma.barrientos@ciencias.unam.mx>
2022-12-13 18:48:04 -0600

    repo invoice updated

--- invoice/src/main/java/com/invoice/api/repository/RepoInvoice.java
+++ invoice/src/main/java/com/invoice/api/repository/RepoInvoice.java
@@ -12,4 +12,12 @@ public interface RepoInvoice extends JpaRepository<Invoice, Integer>{
 
 	List<Invoice> findByRfcAndStatus(String rfc, Integer status);
 
+    /* Firma del método necesario para asignar un invoice_id a los items sin primero
+     * haber guardado un invoice en base de datos. La idea es iterar sobre todas las
+     * facturas (activas o inactivas) y, como el invoice_id es autoincrementado, el
+     * invoice_id de una nueva factura siempre será el valor de la última factura 
+     * mas 1.
+     */
+    @Query(value="SELECT * FROM invoice", nativeQuery=true)
+    List<Invoice> getInvoice();
 }
